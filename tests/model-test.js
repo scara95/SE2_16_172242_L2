e0d254c1f4e7978ab227abb5df2e2592349c9ca5
Nicola Scarabello <nicola.scarabello@studenti.unitn.it>
2016-11-15 16:19:58 +0100

    Tests passed

--- tests/model-test.js
+++ tests/model-test.js
@@ -1,5 +1,6 @@
 var _ = function () {
-    var model = new Model();
+    var model = new Model(13);
+    test(model.check());
     model.add("bulloni", 5);
     test(model.get("bulloni") == 5);
     model.add("bulloni", 3);
@@ -7,4 +8,7 @@ var _ = function () {
     model.add("biscotti", 5);
     test(model.get("biscotti") == 5);
     test(model.get("bulloni") == 8);
+    test(model.check());
+    model.add("bulloni", 6);
+    test(!model.check());
 }();
