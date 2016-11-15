b60a6e6f068950baf95f6d99babcdff39b49b36f
Nicola Scarabello <nicola.scarabello@studenti.unitn.it>
2016-11-15 14:05:10 +0100

    Documenta modello

--- model.js
+++ model.js
@@ -1,11 +1,14 @@
 function Model() {
     this.data = {};
+    //add quantity to thing, if not present create it
     this.add = function (thing, quantity) {
+        //if first insert, initialize the field to 0
         if(!(thing in this.data)) {
             this.data[thing] = 0;
         }
         this.data[thing] += quantity;
     };
+    //get the quantity of thing, undefined if not present
     this.get = function(thing) {
         return this.data[thing];
     };
